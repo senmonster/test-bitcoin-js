7677f5ec833067ae2492f28b52bf0a3dae3bd072
JasonYang <yzsenmonster@gmail.com>
2024-01-25 18:41:10 +0800

    contruct  create commit and reveal tx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import * as ecc from "@bitcoin-js/tiny-secp256k1-asmjs";
 import { Buffer } from "buffer";
 import { broadcast, fetchUtxos } from "./api";
 import { assert } from "console";
+import { buildDataOutput } from "./utils";
 // const livenet = bitcoin.networks.bitcoin;
 const testnet = bitcoin.networks.testnet;
 // const regtest = regtestUtils.network;
@@ -22,10 +23,15 @@ const bip32 = BIP32Factory(ecc);
 
 const toXOnly = (pubKey: Buffer) => (pubKey.length === 32 ? pubKey : pubKey.slice(1, 33));
 
+type RevealTxParams = {
+	commit_tx: bitcoin.Transaction;
+	commit_output: Buffer | undefined;
+	hashForRevealTX: Buffer | undefined;
+};
+
 function App() {
 	const [logs, setLogs] = useState<{
 		address: string | undefined;
-		txid: string | undefined;
 	} | null>(null);
 	const [connected, setConnected] = useState(false);
 	const [content, setContent] = useState("");
@@ -36,61 +42,33 @@ function App() {
 
 	const internalKey = bip32.fromSeed(seed, useNetwork);
 	const btcPrivateKey = internalKey.derivePath(btcPath);
+	const rootAddress = "tb1p7udctqtfn2tw4lnuemslc0zn3tmv3y558ss66w7thq4rqqz94j5sw9c8as";
 
-	const createCommitTx = (keyPairs: BIP32Interface) => {};
-
-	const createRevealTx = () => {};
-
-	const onShowAdr = async () => {
-		// const seed = bip39.mnemonicToSeedSync(memonic);
-		// const internalKey = bip32.fromSeed(seed, regtest);
-		// const leafKey = bip32.fromSeed(seed, regtest);
+	const createCommitTx = async (
+		btcPrivateKey: BIP32Interface,
+		rootAddress: string,
+		leafScriptAsm: string
+	): Promise<RevealTxParams> => {
+		// construct op-false data
 
 		// const leafScriptAsm = `${toXOnly(leafKey.publicKey).toString("hex")} OP_CHECKSIG`;
 		// const leafScript = bitcoin.script.fromASM(leafScriptAsm);
+		// const dataHex = Buffer.from(JSON.stringify(dataOutput)).toString("hex");
 
-		// const scriptTree = {
-		// 	output: leafScript,
-		// };
-
-		// const { output, address, hash } = bitcoin.payments.p2tr({
-		// 	internalPubkey: toXOnly(internalKey.publicKey),
-		// 	scriptTree,
-		// 	network: regtest,
-		// });
-
-		// construct op-false data for utxo output
-		const dataOutput = [
-			"metaid", // chain flag
-			"create", // operation type
-			"/protocols/SimpleBuzz/4", // path to operate, exp:  /protocols/SimpleBuzz  // 4 can use a random number
-			"0", // content的加密类型，0为不加密；1为ECIES加密，2为ECDH协商密钥加密
-			"1.0.0", // version
-			"application/jason", // optional，content-type，default: application/jason
-			"utf8", // optional, encoding, default: utf8
-			JSON.stringify({ content: "Hello Jason World" }), // payload : stringify json body]
-		];
-		const dataHex = Buffer.from(JSON.stringify(dataOutput)).toString("hex");
-		// const leafScriptAsm = JSON.stringify(dataOutput);
-		const leafScript = bitcoin.script.fromASM(dataHex);
+		const leafScript = bitcoin.script.fromASM(leafScriptAsm);
 		const scriptTree = {
 			output: leafScript,
 		};
-		const rootAddress = "tb1p7udctqtfn2tw4lnuemslc0zn3tmv3y558ss66w7thq4rqqz94j5sw9c8as";
+
 		const {
 			address: dataAddress,
-			output,
+			output: commit_output,
 			hash: hashForRevealTX,
 		} = bitcoin.payments.p2tr({
 			internalPubkey: toXOnly(btcPrivateKey.publicKey),
 			network: useNetwork,
 			scriptTree,
 		});
-		const { hash: hashForCommitTX } = bitcoin.payments.p2tr({
-			internalPubkey: toXOnly(btcPrivateKey.publicKey),
-			network: useNetwork,
-		});
-		console.log("hashForCommitTX", hashForCommitTX);
 
 		// ---------------contruct commit commit Psbt--------------
 		const commitPsbt = new bitcoin.Psbt({ network: useNetwork });
@@ -125,18 +103,29 @@ function App() {
 		commitPsbt.signInput(0, tweakedSigner);
 
 		commitPsbt.finalizeAllInputs(); // must have sign,
-		const tx_commit = commitPsbt.extractTransaction();
-		const hex_commit = tx_commit.toHex();
+		const commit_tx = commitPsbt.extractTransaction();
+		const commit_hex = commit_tx.toHex();
 
 		const commitRes = await broadcast({
-			rawTx: hex_commit,
+			rawTx: commit_hex,
 			network: "testnet",
 			publicKey: btcPrivateKey.publicKey.toString("hex"),
 			message: "construct commit tx",
 		});
 		console.log("commit tx res", commitRes);
+		return {
+			commit_tx,
+			commit_output,
+			hashForRevealTX,
+		};
+	};
 
-		// -----------------------contruct commit revealPsbt--------------------
+	const createRevealTx = async (
+		btcPrivateKey: BIP32Interface,
+		rootAddress: string,
+		revealTxParams: RevealTxParams
+	) => {
+		// -----------------------contruct reveal Psbt--------------------
 		const revealPsbt = new bitcoin.Psbt({ network: useNetwork });
 
 		// amount from faucet
@@ -146,55 +135,177 @@ function App() {
 		// get faucet
 
 		revealPsbt.addInput({
-			hash: tx_commit.getId(),
+			hash: revealTxParams.commit_tx.getId(),
 			index: 0,
-			witnessUtxo: { value: amount, script: output! },
+			witnessUtxo: { value: amount, script: revealTxParams.commit_output! },
 			tapInternalKey: toXOnly(btcPrivateKey.publicKey), // toXOnly(internalKey.publicKey),
-			tapMerkleRoot: hashForRevealTX,
+			tapMerkleRoot: revealTxParams.hashForRevealTX,
 		});
 		revealPsbt.addOutput({ value: sendAmount, address: rootAddress! });
 
 		const tweakedSigner2 = btcPrivateKey.tweak(
 			bitcoin.crypto.taggedHash(
 				"TapTweak",
-				Buffer.concat([toXOnly(btcPrivateKey.publicKey), hashForRevealTX!])
+				Buffer.concat([toXOnly(btcPrivateKey.publicKey), revealTxParams.hashForRevealTX!])
 			)
 		);
 
 		revealPsbt.signInput(0, tweakedSigner2);
 
 		revealPsbt.finalizeAllInputs();
-		const tx_reveal = revealPsbt.extractTransaction();
-		const hex_reveal = tx_reveal.toHex();
+		const reveal_tx = revealPsbt.extractTransaction();
+		const reveal_hex = reveal_tx.toHex();
 
 		const revealRes = await broadcast({
-			rawTx: hex_reveal,
+			rawTx: reveal_hex,
 			network: "testnet",
 			publicKey: btcPrivateKey.publicKey.toString("hex"),
 			message: "construct reveal tx",
 		});
 
 		console.log("reveal tx res", revealRes);
+		return reveal_hex;
+	};
 
-		setLogs({ address: rootAddress, txid: tx_reveal.getId() });
+	const onConnect = async () => {
+		// const seed = bip39.mnemonicToSeedSync(memonic);
+		// const internalKey = bip32.fromSeed(seed, regtest);
+		// const leafKey = bip32.fromSeed(seed, regtest);
+		// const leafScriptAsm = `${toXOnly(leafKey.publicKey).toString("hex")} OP_CHECKSIG`;
+		// const leafScript = bitcoin.script.fromASM(leafScriptAsm);
+		// const scriptTree = {
+		// 	output: leafScript,
+		// };
+		// const { output, address, hash } = bitcoin.payments.p2tr({
+		// 	internalPubkey: toXOnly(internalKey.publicKey),
+		// 	scriptTree,
+		// 	network: regtest,
+		// });
+		// --------------------------------------------------------
+		// ---------------contruct commit Psbt--------------
+		// const commitPsbt = new bitcoin.Psbt({ network: useNetwork });
+		// const faucetUtxos = await fetchUtxos({ address: rootAddress, network: "testnet" });
+		// const toUseUtxo = faucetUtxos[0]; // presume toUseUtxo.value >= 11546
+		// console.log("to use utxo", toUseUtxo);
+		// assert(toUseUtxo.satoshi > 11546 + 546, true);
+		// commitPsbt.addInput({
+		// 	hash: toUseUtxo.txId,
+		// 	index: toUseUtxo.vout,
+		// 	witnessUtxo: {
+		// 		value: toUseUtxo.satoshi,
+		// 		script: bitcoin.address.toOutputScript(rootAddress!, useNetwork),
+		// 	}, //lock script
+		// 	tapInternalKey: toXOnly(btcPrivateKey.publicKey), // toXOnly(internalKey.publicKey),
+		// });
+		// commitPsbt.addOutput({
+		// 	value: 5546,
+		// 	address: dataAddress!,
+		// });
+		// // if change > 546
+		// commitPsbt.addOutput({
+		// 	value: toUseUtxo.satoshi - 5546 - 6000,
+		// 	address: rootAddress!,
+		// });
+		// // no need hash from commit tx
+		// const tweakedSigner = btcPrivateKey.tweak(
+		// 	bitcoin.crypto.taggedHash("TapTweak", Buffer.concat([toXOnly(btcPrivateKey.publicKey)]))
+		// );
+		// commitPsbt.signInput(0, tweakedSigner);
+		// commitPsbt.finalizeAllInputs(); // must have sign,
+		// const commit_tx = commitPsbt.extractTransaction();
+		// const commit_hex = commit_tx.toHex();
+		// const commitRes = await broadcast({
+		// 	rawTx: commit_hex,
+		// 	network: "testnet",
+		// 	publicKey: btcPrivateKey.publicKey.toString("hex"),
+		// 	message: "construct commit tx",
+		// });
+		// console.log("commit tx res", commitRes);
+		// // -----------------------contruct reveal Psbt--------------------
+		// const revealPsbt = new bitcoin.Psbt({ network: useNetwork });
+		// // amount from faucet
+		// const amount = 5546; // 42
+		// const sendAmount = amount - 5000; // 5000 is gas for reveal tx
+		// // get faucet
+		// revealPsbt.addInput({
+		// 	hash: commit_tx.getId(),
+		// 	index: 0,
+		// 	witnessUtxo: { value: amount, script: output! },
+		// 	tapInternalKey: toXOnly(btcPrivateKey.publicKey), // toXOnly(internalKey.publicKey),
+		// 	tapMerkleRoot: hashForRevealTX,
+		// });
+		// revealPsbt.addOutput({ value: sendAmount, address: rootAddress! });
+		// const tweakedSigner2 = btcPrivateKey.tweak(
+		// 	bitcoin.crypto.taggedHash(
+		// 		"TapTweak",
+		// 		Buffer.concat([toXOnly(btcPrivateKey.publicKey), hashForRevealTX!])
+		// 	)
+		// );
+		// revealPsbt.signInput(0, tweakedSigner2);
+		// revealPsbt.finalizeAllInputs();
+		// const reveal_tx = revealPsbt.extractTransaction();
+		// const reveal_hex = reveal_tx.toHex();
+		// const revealRes = await broadcast({
+		// 	rawTx: reveal_hex,
+		// 	network: "testnet",
+		// 	publicKey: btcPrivateKey.publicKey.toString("hex"),
+		// 	message: "construct reveal tx",
+		// });
+		// console.log("reveal tx res", revealRes);
+
+		setLogs({ address: rootAddress });
 		setConnected(true);
+	};
+
+	const onInitMetaid = async () => {
+		const leafScriptAsm_init = buildDataOutput({
+			publicKey: toXOnly(btcPrivateKey.publicKey).toString("hex"),
+			operation: "init",
+		});
+
+		const initRevealTxParams = await createCommitTx(
+			btcPrivateKey,
+			rootAddress,
+			leafScriptAsm_init
+		);
+		// const initRevealHex = await createRevealTx(btcPrivateKey, rootAddress, initRevealTxParams);
+	};
+
+	const onTestBuzz = async () => {
+		const leafScriptAsm_buzz = buildDataOutput({
+			publicKey: toXOnly(btcPrivateKey.publicKey).toString("hex"),
+			operation: "create",
+			path: "/protocols/SimpleBuzz/4",
+			body: { content: "JSON IS COMING!!!" },
+		});
+
+		const buzzRevealTxParams = await createCommitTx(
+			btcPrivateKey,
+			rootAddress,
+			leafScriptAsm_buzz
+		);
+		const buzzRevealHex = await createRevealTx(btcPrivateKey, rootAddress, buzzRevealTxParams);
 	};
 
 	const onSendBuzz = () => {};
 	return (
 		<div className="">
 			{connected ? (
-				<Button
-					variant="outlined"
-					onClick={() => {
-						setConnected(false);
-						setLogs(null);
-					}}
-				>
-					disnnect wallet
-				</Button>
+				<div className="flex gap-2 place-content-center">
+					<Button
+						variant="outlined"
+						onClick={() => {
+							setConnected(false);
+							setLogs(null);
+						}}
+					>
+						disnnect wallet
+					</Button>
+					<Button onClick={onInitMetaid}>Init Metaid</Button>
+					<Button onClick={onTestBuzz}>Send Buzz</Button>
+				</div>
 			) : (
-				<Button variant="contained" onClick={onShowAdr}>
+				<Button variant="contained" onClick={onConnect}>
 					connect wallet
 				</Button>
 			)}
--- src/utils.ts
+++ src/utils.ts
@@ -1,40 +1,68 @@
 export type Operation = "init" | "create" | "modify" | "revoke";
 export type Encryption = "0" | "1" | "2";
-type MetaidDataOutput = [
-	"metaid", // chain flag
-	Operation, // operation type
-	string, // path to operate, exp: /root/protocols/SimpleBuzz
-	Encryption, // content的加密类型，0为不加密；1为ECIES加密，2为ECDH协商密钥加密
-	string, // version
-	string, // optional，content-type，default: application/jason
-	string, // optional, encoding, default: utf8
-	string // payload : stringify json body
-];
+// type MetaidDataOutput = [
+// 	"metaid", // chain flag
+// 	Operation, // operation type
+// 	string, // path to operate, exp: /root/protocols/SimpleBuzz
+// 	Encryption, // content的加密类型，0为不加密；1为ECIES加密，2为ECDH协商密钥加密
+// 	string, // version
+// 	string, // optional，content-type，default: application/jason
+// 	string, // optional, encoding, default: utf8
+// 	string // payload : stringify json body
+// ];
+
+const toHexString = (s: string) => {
+	return Buffer.from(s).toString("hex");
+};
 
 export function buildDataOutput({
+	publicKey,
 	operation,
 	path,
-	encryption,
+	encryption = "0",
+	version = "1.0.0",
 	body,
 	dataType = "application/json",
+
 	encoding = "UTF-8",
 }: {
+	publicKey: string;
 	operation: Operation;
-	path: string;
-	encryption: "0" | "1" | "2";
-	body: unknown;
+	path?: string;
+	encryption?: "0" | "1" | "2";
+	version?: string;
+	body?: unknown;
 	dataType?: string;
 	encoding?: string;
 }) {
-	const dataOutput: MetaidDataOutput = [
-		"metaid", // chain flag
-		operation, // operation type
-		path, // path to operate, exp:  /protocols/SimpleBuzz
-		encryption, // content的加密类型，0为不加密；1为ECIES加密，2为ECDH协商密钥加密
-		"1.0.0", // version
-		dataType, // optional，content-type，default: application/jason
-		encoding, // optional, encoding, default: utf8
-		JSON.stringify(body), // payload : stringify json body]
+	const leafScriptAsmArray = [
+		publicKey,
+		"OP_CHECKSIG",
+		"OP_FALSE",
+		"OP_IF",
+		toHexString("metaid"),
+		toHexString(operation),
 	];
-	return dataOutput;
+	if (operation !== "init") {
+		leafScriptAsmArray.push(toHexString(path!));
+		leafScriptAsmArray.push(toHexString(encryption));
+		leafScriptAsmArray.push(toHexString(version));
+		leafScriptAsmArray.push(toHexString(dataType));
+		leafScriptAsmArray.push(toHexString(JSON.stringify(body!)));
+	}
+	leafScriptAsmArray.push("OP_ENDIF");
+	// const leafScriptAsm = `${toXOnly(leafKey.publicKey).toString("hex")} OP_CHECKSIG`;
+	// const leafScript = bitcoin.script.fromASM(leafScriptAsm);
+
+	// const dataOutput: MetaidDataOutput = [
+	// 	"metaid", // chain flag
+	// 	operation, // operation type
+	// 	path, // path to operate, exp:  /protocols/SimpleBuzz
+	// 	encryption, // content的加密类型，0为不加密；1为ECIES加密，2为ECDH协商密钥加密
+	// 	"1.0.0", // version
+	// 	dataType, // optional，content-type，default: application/jason
+	// 	encoding, // optional, encoding, default: utf8
+	// 	JSON.stringify(body), // payload : stringify json body]
+	// ];
+	return leafScriptAsmArray.join(" ");
 }
